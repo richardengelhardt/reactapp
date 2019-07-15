5fd28e049470fcb73535af28c5fd5ab0ae8d85b4
Richard Engelhardt <rcs.engelhardt@gmail.com>
2019-07-15 12:06:34 -0400

    chaper 2_v1

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -10,8 +10,25 @@ const header =(
 
 );
 
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Richard
+            </span>
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <span className="counter-score">35</span>
+                <button className="counter-action increment"> + </button>
+            </div>
+
+        </div>
+
+    );
+}
+
 ReactDOM.render(
- header,
+ <Player />,
  document.getElementById('root')
 );
 
